c78a2fac5aefc697c68c36cfbb262993d80e667c
CuriousGeek <krit.s255@gmail.com>
2020-09-30 00:01:17 -0400

    minor remarks added in script.js

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
-function showloadingSpinner(){
+function showloadingSpinner() {
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
-function hideLoadingSpinner(){
-    if(!loader.hidden){
+function hideLoadingSpinner() {
+    if (!loader.hidden) {
         quoteContainer.hidden = false;
         loader.hidden = true;
     }
@@ -20,22 +20,22 @@ function hideLoadingSpinner(){
 }
 
 //Get quote from API
-async function getQuote(){
+async function getQuote() {
     const proxyURL = 'https://cors-anywhere.herokuapp.com/';
     const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-    try{
+    try {
         showloadingSpinner();
         const response = await fetch(proxyURL + apiURL);
         const data = await response.json();
         //if author field is null, add 'Unknown'
-        if(data.quoteAuthor === ''){
+        if (data.quoteAuthor === '') {
             quoteAuthor.innerText = '-Unknown'
-        } else{
-            quoteAuthor.innerText = '-'+data.quoteAuthor;
+        } else {
+            quoteAuthor.innerText = '-' + data.quoteAuthor;
         }
-        
+
         //Reduce the font size for long quotes.
-        if(data.quoteText.length > 120){
+        if (data.quoteText.length > 120) {
             quoteText.classList.add('long-quote');
         } else {
             quoteText.classList.remove('long-quote');
@@ -43,16 +43,17 @@ async function getQuote(){
         quoteText.innerText = data.quoteText;
         //Quote fetch is complete so hide loading
         hideLoadingSpinner();
-    }catch(error){
+    } catch (error) {
+        //Recursive call... BecareFul with it.
         getQuote();
-    }  
+    }
 }
 //Tweet quote on twitter page.
-function tweetQuote(){
+function tweetQuote() {
     const quote = quoteText.innerText;
     const author = quoteAuthor.innerText;
     const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
-    window.open(twitterUrl,'_blank');
+    window.open(twitterUrl, '_blank');
 }
 
 //Event Listners
